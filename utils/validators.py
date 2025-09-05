6e3e38f2d22f2bd7edc777ca5ac4ff1625fe682b
VINEET YADAV <vineet9949@gmail.com>
2025-09-05 15:20:56 +0530

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -5,27 +5,27 @@ import streamlit as st
 def validate_positive_number(value, field_name):
     """Validate that a number is positive"""
     if value <= 0:
-        st.error(f"{field_name} must be greater than 0")
+        st.error(f"{field_name} must be positive")
         return False
     return True
 
 def validate_percentage(value, field_name):
-    """Validate percentage input (0-100)"""
-    if value < 0 or value > 100:
+    """Validate percentage values"""
+    if not (0 <= value <= 100):
         st.error(f"{field_name} must be between 0 and 100")
         return False
     return True
 
 def validate_years(value):
-    """Validate number of years"""
-    if value <= 0 or value > 100:
-        st.error("Number of years must be between 1 and 100")
+    """Validate year inputs"""
+    if not (1 <= value <= 50):
+        st.error("Years must be between 1 and 50")
         return False
     return True
 
-def validate_salvage_vs_purchase(salvage_value, purchase_cost):
-    """Validate that salvage value is less than purchase cost"""
-    if salvage_value >= purchase_cost:
+def validate_salvage_vs_purchase(salvage, purchase):
+    """Validate salvage value vs purchase cost"""
+    if salvage >= purchase:
         st.error("Salvage value must be less than purchase cost")
         return False
     return True
